b86ebdc076f0bc8cc8d7b32aaec916b25241149b
AnceTetere <ance.tetere@outlook.com>
2022-11-25 18:00:09 +0200

    Adding the paste command to the PlayWithDataTypes file.

--- Trial1/PlayWithRDataTypes.R
+++ Trial1/PlayWithRDataTypes.R
@@ -26,4 +26,27 @@
   typeof(q1)
   q2 <- FALSE  
   typeof(q2)  
+
+  A <- 10
+  B <- 5
+  
+  C <- A + B
+  C
+  
+  # Variable 1
+  var1 <- 2.5 
+  
+  # Variable 2
+  var2 <- 4
+  
+  result <- var1/var2
+  result  
+  
+  answer <- sqrt(var2)  
+  answer  
+  
+  greeting <- "Hello"
+  name <- "Bob"
+  message <- paste(greeting, name)
+  message
   
