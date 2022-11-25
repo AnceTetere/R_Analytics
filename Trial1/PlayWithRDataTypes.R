e3c1f913af3f625c77fe26375a5b527b6dcfbc2e
AnceTetere <ance.tetere@outlook.com>
2022-11-25 17:25:52 +0200

    Refresher on R data types

---
+++ Trial1/PlayWithRDataTypes.R
@@ -0,0 +1,29 @@
+
+
+
+# integer
+  x <- 2L
+  typeof(x)
+
+# integer
+  x <- 2 #will be stored as double
+  typeof(x)
+  
+# double
+  y <- 2.5
+  typeof(y)  
+  
+# complex
+  z <- 3 + 2i
+  typeof(z)
+  
+# character
+  a <- "h"
+  typeof(a)
+  
+# logical
+  q1 <- T
+  typeof(q1)
+  q2 <- FALSE  
+  typeof(q2)  
+  
