778233b0a4f289be26ef26f9ca5fa159d2c2a3e8
AnceTetere <ance.tetere@outlook.com>
2022-11-26 20:15:46 +0200

    Adding logical operators scriot.

---
+++ Trial1/LogicalOperators.R
@@ -0,0 +1,32 @@
+result | result2
+result & result2
+isTRUE(result)
+
+result2 <- !TRUE
+result2
+
+result3 <- !(5 > 1)
+result3
+
+# Logical
+# TRUE T
+# FALSE F
+
+4 < 5
+10 > 100
+4 == 5
+
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# !
+# &
+# isTRUE(X)
+
+result <- 4 < 5
+result
+typeof(result)
+
