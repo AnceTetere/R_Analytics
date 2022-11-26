48b3aeb14bdc030910b64e0dae57a58f01c9b65f
AnceTetere <ance.tetere@outlook.com>
2022-11-26 20:05:45 +0200

    Added some character strings.

--- Trial1/PlayWithRDataTypes.R
+++ Trial1/PlayWithRDataTypes.R
@@ -1,5 +1,23 @@
+greating <- "Welcome,"
+name <- "Ineta"
+message <- paste(greating, name)
+message
 
+A <- 10
+B <- 5
+C <- A + B
+C
 
+# variable 1
+var1 <- 2.5
+# variable 2
+var2 <- 4
+
+result <- var1 / var2
+result
+
+answer <- sqrt(var2)
+answer
 
 # integer
   x <- 2L
