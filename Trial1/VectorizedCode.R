321d675248ac599add79276e0204586991d5c322
AnceTetere <ance.tetere@outlook.com>
2022-12-18 13:42:41 +0200

    Minor example additions for VectorizedCode script

---
+++ Trial1/VectorizedCode.R
@@ -0,0 +1,43 @@
+
+
+MyFirstVector <- c(3, 45, 56, 732)
+MyFirstVector
+
+is.numeric(MyFirstVector)
+is.integer(MyFirstVector)     # Default for R is double
+is.double(MyFirstVector)
+
+V2 <- c(3L, 12L, 243L, 0L)
+is.numeric(V2)
+is.integer(V2)
+is.double(V2)
+
+V3 <- c("a", "B23", "Hello")
+V3
+is.character(V3)
+is.numeric(V3)
+
+V4 <- c("a", "B23", "Hello", 7)   # R will use coercion for 7 into a character type
+V4
+is.character(V4)
+is.numeric(V4)
+
+seq() # sequence - like ":"
+rep()  # replicate
+
+seq(1, 15)
+1:15
+
+seq(1, 15, 2)
+z <- seq(1, 15, 4)
+z
+
+rep(3, 50)
+d <- rep(3, 50)
+d
+
+rep("a", 5)
+x <- c(80, 20)
+y <- rep(x, 10)
+y
+
