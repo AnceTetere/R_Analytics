32456e5eb8576626e7ef07209aed44765cb77f6f
AnceTetere <ance.tetere@outlook.com>
2022-12-18 14:41:26 +0200

    Update to the Vectorized code script

--- Trial1/VectorizedCode.R
+++ Trial1/VectorizedCode.R
@@ -1,5 +1,48 @@
 
 
+
+MyFirstVector <- c(3, 45, 56, 732)
+MyFirstVector
+
+is.numeric(MyFirstVector)
+is.integer(MyFirstVector)
+is.double(MyFirstVector)
+
+V2 <- c(3L, 12L, 243L, 0L)
+is.numeric(V2)
+is.integer(V2)
+is.double(V2)
+
+V3 <- c("a", "B23", "Hello")
+V3
+is.character(V3)
+is.numeric(V3)
+
+V4 <- c("a", "B23", "Hello", 7)
+V4
+is.character(V4)
+is.numeric(V4)
+
+seq() # sequence - like ':'
+rep() # replicate
+
+seq(1, 15)
+1:15
+seq(1, 15, 2)
+z <- seq(1, 15, 4)
+z
+
+rep(3, 50)
+d <- rep(3, 50)
+d
+
+rep("a", 5)
+x <- c(80, 20)
+y <- rep(x, 10)
+y
+
+
+###
 MyFirstVector <- c(3, 45, 56, 732)
 MyFirstVector
 
@@ -17,7 +60,8 @@ V3
 is.character(V3)
 is.numeric(V3)
 
-V4 <- c("a", "B23", "Hello", 7)   # R will use coercion for 7 into a character type
+V4 <-
+  c("a", "B23", "Hello", 7)   # R will use coercion for 7 into a character type
 V4
 is.character(V4)
 is.numeric(V4)
