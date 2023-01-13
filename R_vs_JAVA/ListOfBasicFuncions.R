83433aaf59451038d283d99d752d85ae3a757bec
AnceTetere <ance.tetere@outlook.com>
2023-01-13 09:24:16 +0200

    Adding a list of basic functions with variations.

---
+++ R_vs_JAVA/ListOfBasicFuncions.R
@@ -0,0 +1,31 @@
+# ?
+
+rnorm(n = 5, sd = 8)
+
+c()
+
+seq(from = 10, to = 20, by = 3)
+seq(from = 10, to = 20, length.out = 100)
+
+x <- c("a", "b", "c")
+seq(from = 10, to = 20, along.with = x)
+A <- seq(from = 10, to = 20, along.with = x)
+
+rep(5:6, times = 10)
+rep(5:6, each = 10)
+rep(x, times = 5)
+rep(x, each = 5)
+
+print()
+
+is.numeric()
+is.integer()
+is.double()
+is.character()
+
+typof()
+
+A
+B<- sqrt(A)
+B
+paste()
