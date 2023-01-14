44b3a4417533112c009dace4157f4450ff8145b7
AnceTetere <ance.tetere@outlook.com>
2023-01-14 08:02:11 +0200

    Update to the ListOfBasicFunctions script.

--- R_vs_JAVA/ListOfBasicFuncions.R
+++ R_vs_JAVA/ListOfBasicFuncions.R
@@ -1,3 +1,41 @@
+rnorm(n = 5, mean = 10, sd = 3)
+dnorm(x = 5, mean = 10, sd = 3, log = TRUE)
+pnorm(q = 5,mean = 10, sd = 3, lower.tail = TRUE, log.p = TRUE)
+
+seq(from = 10, to = 20, by = 3)
+seq(from = 10, to = 20, length.out = 10)
+
+x <- c("a", "b", "c")
+seq(from = 10, to = 20, along.with = x)
+
+rep(5:6, times = 10)
+rep(5:6, each = 10)
+rep(x, each = 5)
+rep(x, times = 5)
+
+A <- seq(from = 10, to = 20, along.with = x)
+B <- sqrt(A)
+B
+##################
+
+rnorm()
+
+c()
+seq()
+?rep()
+
+print()
+
+is.numeric()
+is.integer()
+is.double()
+is.character()
+
+typeof()
+
+sqrt()
+paste()
+
 # ?
 
 rnorm(n = 5, sd = 8)
