5efc823419e314c519b623c7e175ecab8de2e9ad
AnceTetere <ance.tetere@outlook.com>
2023-01-15 13:54:56 +0200

    A small update to the functions list.

--- R_vs_JAVA/ListOfBasicFuncions.R
+++ R_vs_JAVA/ListOfBasicFuncions.R
@@ -1,3 +1,27 @@
+rnorm(5, sd = 8)
+
+x <- c("a", "b", "c")
+c()
+A <- seq(from = 10, to = 20, along.with = x)
+rep(x, 2)
+rep(5:6, times = 10)
+rep(5:6, each = 10)
+
+print()
+
+is.numeric()
+is.integer()
+is.double()
+is.character()
+
+typeof()
+
+A
+B <- sqrt(A)
+B
+paste()
+
+
 rnorm(n = 5, mean = 10, sd = 3)
 dnorm(x = 5, mean = 10, sd = 3, log = TRUE)
 pnorm(q = 5,mean = 10, sd = 3, lower.tail = TRUE, log.p = TRUE)
