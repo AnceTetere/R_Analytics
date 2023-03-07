9e408dd5880603c7cab65f643729f12938f00c30
AnceTetere <ance.tetere@outlook.com>
2023-03-07 04:57:09 +0200

    Doing some visualisations with Matplot.

--- Visualisations/Code/1_Matplot.R
+++ Visualisations/Code/1_Matplot.R
@@ -1,3 +1,29 @@
+#Salary
+myplot(Salary)
+myplot(Salary/Games)
+myplot(Salary/FieldGoals)
+
+#In-Game Metrics
+myplot(MinutesPlayed)
+myplot(Points)
+
+#In-Game Metrics Normalized
+myplot(FieldGoals/Games)
+myplot(FieldGoals/FieldGoalAttempts)
+myplot(FieldGoalAttempts/Games)
+myplot(Points/Games)
+
+#Interesting Observation
+myplot(MinutesPlayed/Games)
+myplot(Games)
+
+#Time is valuable
+myplot(FieldGoals/MinutesPlayed)
+
+#Player Style
+myplot(Points/FieldGoals)
+
+
 myplot <- function(data, rows = 1:10){
   Data <- Games[rows, , drop = F]
   matplot(t(Data), type = "b", pch = 15:18, col = c(1:4, 6))
