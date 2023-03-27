ec659dc7e05f876229ba93e2e1a0ecb14556d5d4
AnceTetere <ance.tetere@outlook.com>
2023-03-27 13:17:56 +0300

    Writing to an Excel sheet from R.

--- Trial1/WritingToXLsheet.R
+++ Trial1/WritingToXLsheet.R
@@ -4,4 +4,8 @@ library(XLConnect)
 getwd()
 
 wb <- loadWorkbook("file.xlsx", create = TRUE)
+createSheet(wb, "Sheet1")
+
+writeWorksheet(object = wb, data = stats, sheet = "Sheet1", 
+               startRow = 1, startCol = 1, header = TRUE)
 
