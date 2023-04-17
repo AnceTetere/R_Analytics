8d600f1e7543ee62139f1ce40f66b426b80775a8
AnceTetere <ance.tetere@outlook.com>
2023-04-17 17:32:15 +0300

    Adding mapping to the qplot()

--- Visualisations/Code/2_QplotIntro.R
+++ Visualisations/Code/2_QplotIntro.R
@@ -13,3 +13,12 @@ qplot(data = stats,x=Income.Group, y=Birth.rate,
 qplot(data = stats,x=Income.Group, y=Birth.rate, 
       size = I(3), colour=I("blue"))
 qplot(data=stats, x=Income.Group, y=Birth.rate, geom="boxplot")
+
+#--------------------------------Visualizing what we need
+qplot(data=stats, x=Internet.users, y=Birth.rate)
+qplot(data=stats, x=Internet.users, y=Birth.rate,
+      colour=I("red"), size=I(4))
+
+#Doing the mapping
+qplot(data=stats, x=Internet.users, y=Birth.rate,
+      colour=Income.Group, size=I(3))
